2b919c05517a3c3e538c7e92bc193cfd4a1ff592
jaydeep <jaydeepprajapati243@gmail.com>
2026-05-03 14:30:49 +0530

    feat: configure redux store with redux toolkit

---
+++ src/app/store.js
@@ -0,0 +1,15 @@
+// src/app/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import productsReducer from '../features/products/productsSlice';
+import cartReducer from '../features/cart/cartSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    products: productsReducer,
+    cart: cartReducer,
+  },
+});
+
+export default store;
